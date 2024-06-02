b516e73c91b5befd4810f66782d8cb393f352669
OluwaseunAI <olayemifrancisadekogbe@gmail.com>
2024-06-02 11:54:15 +0100

    pushing local code

--- guess-ng.js
+++ guess-ng.js
@@ -3,6 +3,7 @@ const enterButton = document.querySelector('.button');
 const outputPara = document.querySelector('.output-p');
 const attemptsSpan = document.querySelector('.output span');
 
+
 //Generate a random number using Math.floor(Math.random
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -26,7 +27,7 @@ enterButton.addEventListener('click', guessNumber);
 function guessNumber() {
 
 
-   const arrayQuirkySet = ["Oops, you've been gamed! \u{1F929}", "I win, you lose \u{1F971}", "Try Again, Try not to get the L \u{1F602}", "This time you're no g\*\*d! \u{1F616}", "Is this your best\? Pretty sure it ain\'t u{1F601}"];
+   const arrayQuirkySet = ["Oops, you've been gamed! \u{1F929}", "I win, you lose \u{1F971}", "Try Again, Try not to get the L \u{1F602}", "This time you're no g\*\*d! \u{1F616}", "Is this your best? Pretty sure it ain\'t \u{1F601}"];
  const arrayCongratSet = ["You bested me \u{1F910} \n Congrats, anyway!", "Big up, Number King \u{1F644}", "Spot on, Punters watch this man \u{1F612}", "Right there, we have an octopus in u \u{1F621}", "How did u guess that,\nUghh.. Congrats tho \u{1F611}"];
 
     let userGuess = parseInt(inputField.value);
@@ -38,10 +39,11 @@ function guessNumber() {
      console.log(attemptNo);
 
       // function that checks if the user entered a number
-      if (isNaN(userGuess) || isFinite(userGuess)) {
+      if (isNaN(userGuess) || !isFinite(userGuess) || inputField.value.trim() == "") {
           // alert('Please enter a valid number');
+          outputPara.style.color =  'red';
           outputPara.textContent = "Please enter a valid number";
-        //  console.log(attemptNo)
+        //console.log(attemptNo)
          return;
 
       }
@@ -54,16 +56,17 @@ function guessNumber() {
 
           let randCongratzMsg = arrayCongratSet[Math.floor(Math.random() * arrayCongratSet.length)];
 
-          attemptsSpan.textContent = `You have ${attemptNo} attempt(s) left`;
+          // attemptsSpan.textContent = `You have ${attemptNo} attempt(s) left`;
 
           // alert(`${randCongratzMsg}, Congrats! You guessed the number ${secretNumber} with ${attemptNo} attempt(s) to spare`);
-          outputPara.textContent = `${randCongratzMsg}, Congrats! You guessed the number ${secretNumber} with ${attemptNo} attempt(s) to spare u{2714}`
+          outputPara.style.color =  'green';
+          outputPara.textContent = `\u{2714} ${randCongratzMsg}, Congrats! You guessed the number ${secretNumber} with ${attemptNo} attempt(s) to spare `
           console.log(attemptNo)
           inputField.disabled = true;
           enterButton.disabled = true;
       } else if (attemptNo > 0) {
         console.log(attemptNo)
-        const message = userGuess < secretNumber ? 'Too Low, Try again \u{274C}' : 'Too High, Try again an \u{274C}';
+        const message = userGuess < secretNumber ? 'Too Low, Try again \u{274C}' : 'Too High, Try again ah \u{274C}';
     outputPara.textContent = message;
     console.log(attemptNo)
 
